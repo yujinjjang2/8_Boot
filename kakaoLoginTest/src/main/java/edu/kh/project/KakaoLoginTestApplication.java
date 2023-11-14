f219929615281cd9addb5eebf09e9c1c8018829a
yujinjjang2 <o3odw98@gamil.com>
2023-11-15 00:05:09 +0900

    update home

---
+++ kakaoLoginTest/src/main/java/edu/kh/project/KakaoLoginTestApplication.java
@@ -0,0 +1,13 @@
+package edu.kh.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoLoginTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoLoginTestApplication.class, args);
+	}
+
+}
