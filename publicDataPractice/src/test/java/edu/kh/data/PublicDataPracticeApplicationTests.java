06e85b3d9e6545fd165264ed2804789e0e903c48
yujinjjang2 <o3odw98@gamil.com>
2023-12-18 23:04:29 +0900

    update home

---
+++ publicDataPractice/src/test/java/edu/kh/data/PublicDataPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kh.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicDataPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ publicDataPractice2/src/main/java/edu/kh/data/PublicDataPractice2Application.java
@@ -0,0 +1,13 @@
+package edu.kh.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicDataPractice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicDataPractice2Application.class, args);
+	}
+
+}
