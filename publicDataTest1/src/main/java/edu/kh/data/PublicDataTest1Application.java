583afc729a950b6fcee712bd65565af77d26bc38
yujinjjang2 <o3odw98@gmail.com>
2023-12-19 11:02:42 +0900

    update aca

---
+++ publicDataTest1/src/main/java/edu/kh/data/PublicDataTest1Application.java
@@ -0,0 +1,13 @@
+package edu.kh.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicDataTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicDataTest1Application.class, args);
+	}
+
+}
---
+++ publicDataTest1/src/main/java/edu/kh/data/controller/APIController.java
@@ -0,0 +1,62 @@
+package edu.kh.data.controller;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class APIController {
+	
+	// final 상수로 생성 --> 중요!!!!!!!!!
+	private static final String serviceKey = "rr7vDsNjY%2F1sl182O8PxBHK1k4QvaXByIKZ3mzrkDSeYvb8AONTfQ2YI%2B7Cn%2FFCQURQy%2BDFcKClQivvtQzvQBw%3D%3D";
+
+	@ResponseBody
+	@RequestMapping(value="place", produces="application/json; charset=utf-8") // application/json 형식, 인코딩 utf-8
+	public String place() throws Exception{ // url 때문에 IO Exception 발생해서 넘겨주는 것!
+		
+		// url 만드는 것 --> 중요!!!!!!!!
+		String url = "http://apis.data.go.kr/1741000/TsunamiShelter3/getTsunamiShelter1List";
+		// 공공데이터 항목명과 동일해야 함!
+		url += "?serviceKey=" + serviceKey;
+		url += "&pageNo=1";
+		url += "&numOfRows=2";
+		url += "&type=xml";
+
+		/*
+		 * http://apis.data.go.kr/1613000/BusRouteInfoInqireService/getRouteAcctoThrghSttnList?serviceKey=rr7vDsNjY%2F1sl182O8PxBHK1k4QvaXByIKZ3mzrkDSeYvb8AONTfQ2YI%2B7Cn%2FFCQURQy%2BDFcKClQivvtQzvQBw%3D%3D
+		 * &cityCode=25&routeId=DJB30300004&_type=json
+		 * 
+		 * */
+		
+		URL requestUrl = new URL(url); // url 객체 생성
+
+		HttpURLConnection urlConnection = (HttpURLConnection)requestUrl.openConnection();
+
+		urlConnection.setRequestMethod("GET"); // 요청메소드 GET 설정
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+		String responseData = ""; // 응답데이터 변수 초기화
+		String line; 
+
+		while((line = br.readLine()) != null) {
+			responseData += line;
+		}
+		
+		br.close(); // BufferedReader 닫는 것
+
+		urlConnection.disconnect(); // url 연결 끊는 것
+
+		System.out.println("responseData::" + responseData);
+				
+		return responseData;
+		
+	}
+	
+}
+
