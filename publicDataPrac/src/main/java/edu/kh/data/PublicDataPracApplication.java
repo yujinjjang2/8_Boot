b67374502375c2f36855d6d18cc2cef792cd09ae
yujinjjang2 <o3odw98@gamil.com>
2023-12-19 23:25:50 +0900

    update home

---
+++ publicDataPrac/src/main/java/edu/kh/data/PublicDataPracApplication.java
@@ -0,0 +1,13 @@
+package edu.kh.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicDataPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicDataPracApplication.class, args);
+	}
+
+}
